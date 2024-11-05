025c0162db75b5c9cd2bf577412cb6889967f531
HwangRock <157935545+HwangRock@users.noreply.github.com>
2024-11-05 19:23:52 +0900

    create springproject
    create springproject

---
+++ src/test/java/com/example/rockcafe/RockcafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rockcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockcafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
